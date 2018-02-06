1d24111599f6aad0cf5d3f4a1fd14f20e40a6142
Niles Tanner <niles.tanner@gmail.com>
2018-02-05 20:55:19 -0600

    first commit working prototype

---
+++ index.js
@@ -0,0 +1,42 @@
+const textAnalyzer = {};
+
+textAnalyzer.analyzer = (input) => {
+  let hash = {};
+  let output = {
+    count: 0,
+    unique: 0
+  };
+  const addToMap = (word) => {
+    if (word !== '') {
+      word = word.toLowerCase();
+      if (hash[word] !== undefined) {
+        hash[word].count++
+      } else {
+        output.unique++;
+        hash[word] = {
+          text: word,
+          count: 1
+        };
+      }
+    }
+  };
+
+  let words = input.match(/[a-z'\-]+/gi);
+
+  words.forEach((word) => {
+    addToMap(word);
+  });
+  const hashKeys = Object.keys(hash);
+  output.count = hashKeys.length;
+  output.words = hashKeys.map((key) => {
+    hash[key].percent = hash[key].count/output.count;
+    return hash[key];
+  }).sort((a, b) => {
+    return b.count - a.count;
+  });
+
+
+  return output;
+}
+
+module.export = textAnalyzer
